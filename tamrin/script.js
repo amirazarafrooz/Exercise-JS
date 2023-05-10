be98110aa6e0fef6027726870d95ac1a5bad85e0
amirazarafrooz <127439167+amirazarafrooz@users.noreply.github.com>
2023-05-10 17:15:03 +0330

    Add files via upload

--- tamrin/script.js
+++ tamrin/script.js
@@ -5,20 +5,13 @@ const sentence = "khanom hoseini jJalase akhar dasht sabzi pak mikard yadesh be 
 function oniKePeyvandiMikhast(str = '') { 
 const rawData = str.replaceAll(' ','').split('');
 const data = (rawData.reduce((prev,cur) => {
-// const letters  = cur;
 const count = 1;
     if(!prev[cur]) prev[cur] = 0 + 1
     else prev[cur]=prev[cur]+1;
     return prev;
 }, {}))
-
-const outPut = {}
-for(let i = 0; i < data.length; i++){
-    // console.log(data[i].letters + ' : ' + data[i].count)
-}
 return data
 }
 
 
-// oniKePeyvandiMikhast(sentence);
 console.log(oniKePeyvandiMikhast(sentence));
